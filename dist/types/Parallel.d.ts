d6b1b9f8aca187e8fa56670168a7fe8cfcdf76fb
electricessence <5899455+electricessence@users.noreply.github.com>
2025-08-18 18:36:56 -0700

    feat: add Rollup configuration for ESM and CJS builds
    - Created rollup.config.js for bundling the project with ESM output.
    - Added tests for the Parallel module to ensure expected behavior.
    - Introduced tsconfig.cjs.json for CommonJS build configuration.
    - Updated tsconfig.esm.json for ESM build settings.
    - Refined tsconfig.json to improve type checking and exclude unnecessary files.
    - Added tsconfig.types.json for generating type declarations only.

---
+++ dist/types/Parallel.d.ts
@@ -0,0 +1,44 @@
+/*!
+ * @author electricessence / https://github.com/electricessence/
+ * @license MIT
+ * Originally based upon Parallel.js: https://github.com/adambom/parallel.js/blob/master/lib/parallel.js
+ */
+import { ArrayPromise, PromiseBase, PromiseCollection, TSDNPromise } from '@tsdotnet/promises';
+import { WorkerLike } from '@tsdotnet/threading/dist/WorkerLike';
+export interface ParallelOptions {
+    evalPath?: string;
+    maxConcurrency?: number;
+    allowSynchronous?: boolean;
+    env?: any;
+    envNamespace?: string;
+}
+export type RequireType = string | Function | {
+    name?: string;
+    fn: Function;
+};
+export declare class Parallel {
+    readonly options: ParallelOptions;
+    protected readonly _requiredScripts: string[];
+    protected readonly _requiredFunctions: {
+        name?: string;
+        fn: Function;
+    }[];
+    constructor(options?: ParallelOptions);
+    static get isSupported(): boolean;
+    static maxConcurrency(max: number): Parallel;
+    static options(options?: ParallelOptions): Parallel;
+    static require(...required: RequireType[]): Parallel;
+    static requireThese(required: RequireType[]): Parallel;
+    static startNew<T, U>(data: T, task: (data: T) => U, env?: unknown): PromiseBase<U>;
+    static map<T, U>(data: T[], task: (data: T) => U, env?: unknown): ArrayPromise<U>;
+    require(...required: RequireType[]): this;
+    requireThese(required: RequireType[]): this;
+    startNew<T, U>(data: T, task: (data: T) => U, env?: unknown): TSDNPromise<U>;
+    startLocal<T, U>(data: T, task: (data: T) => U): TSDNPromise<U>;
+    pipe<T, U>(data: T[], task: (data: T) => U, env?: unknown): PromiseCollection<U>;
+    map<T, U>(data: T[], task: (data: T) => U, env?: unknown): ArrayPromise<U>;
+    protected _getWorkerSource(task: Function | string, env?: unknown): string;
+    protected _spawnWorker(task: Function | string, env?: unknown): WorkerLike | undefined;
+    private ensureClampedMaxConcurrency;
+}
+export default Parallel;
